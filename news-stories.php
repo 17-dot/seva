a966d3aed92559a3b68a2626ea553d0d4a548928
17-dot <17ddot@gmail.com>
2025-08-14 01:03:15 +0530

    Chnages

--- news-stories.php
+++ news-stories.php
@@ -22,24 +22,22 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm fixed-top">
         <div class="container">
             <a class="navbar-brand" href="index.html">
-                <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHZpZXdCb3g9IjAgMCA0MCA0MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPGNpcmNsZSBjeD0iMjAiIGN5PSIyMCIgcj0iMjAiIGZpbGw9IiNGRjY5MDAiLz4KPHN2ZyB4PSI4IiB5PSI4IiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIGZpbGw9IndoaXRlIj4KPHA+QUs8L3A+Cjwvc3ZnPgo8L2NpcmNsZT4KPC9zdmc+" alt="Akshaya Patra" class="me-2">
-                <span class="fw-bold text-primary">Akshaya Patra</span>
+                <img src="sankatsaathi.png" alt="Sankat Saathi" class="me-2" style="width:50px; height:auto;">
+                <span class="fw-bold text-sankat" style="color: #e62d26;">Sankat Saathi</span>
             </a>
-            
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a class="nav-link" href="who-we-are.html">Who We Are</a>
-                    </li>
+                    </li> -->
                     <li class="nav-item">
                         <a class="nav-link" href="what-we-do.html">What We Do</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="news-stories.html">News & Stories</a>
+                        <a class="nav-link active" href="news-stories.php">News & Stories</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="how-to-help.html">How To Help</a>
@@ -54,9 +52,8 @@
                         <a class="nav-link" href="blogs.html">Blogs</a>
                     </li>
                 </ul>
-                
                 <div class="d-flex">
-                    <a href="index.html#donate" class="btn btn-primary me-2">Donate Now</a>
+                    <a href="#donate" class="btn btn-primary me-2 ">Donate Now</a>
                     <a href="admin.html" class="btn btn-outline-secondary">Admin</a>
                 </div>
             </div>
